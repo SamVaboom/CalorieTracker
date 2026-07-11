0c828a63b2d7920102ba494a802598bd977df916
SamVaboom <samuel.vabulari@ggs.ch>
2026-07-11 17:49:31 +0200

    Add Compose icon dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,6 +32,7 @@ dependencies {
     implementation(platform("androidx.compose:compose-bom:2024.12.01"))
     implementation("androidx.activity:activity-compose:1.10.0")
     implementation("androidx.compose.material3:material3")
+    implementation("androidx.compose.material:material-icons-extended")
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.lifecycle:lifecycle-runtime-compose:2.8.7")
