32e762732b8ac1ddbc5a31fd18c2c2cfdf6e04cb
SamVaboom <samuel.vabulari@ggs.ch>
2026-07-11 18:49:35 +0200

    Expand Room database for MVP

--- app/src/main/java/com/sam/caloriestreak/data/local/database/CalorieStreakDatabase.kt
+++ app/src/main/java/com/sam/caloriestreak/data/local/database/CalorieStreakDatabase.kt
@@ -2,14 +2,28 @@ package com.sam.caloriestreak.data.local.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import com.sam.caloriestreak.data.local.dao.AppDao
 import com.sam.caloriestreak.data.local.dao.IngredientDao
+import com.sam.caloriestreak.data.local.entity.DailyLogEntity
+import com.sam.caloriestreak.data.local.entity.GroceryItemEntity
 import com.sam.caloriestreak.data.local.entity.IngredientEntity
+import com.sam.caloriestreak.data.local.entity.MealLogEntity
+import com.sam.caloriestreak.data.local.entity.RecipeEntity
+import com.sam.caloriestreak.data.local.entity.RecipeItemEntity
 
 @Database(
-    entities = [IngredientEntity::class],
-    version = 1,
+    entities = [
+        IngredientEntity::class,
+        RecipeEntity::class,
+        RecipeItemEntity::class,
+        MealLogEntity::class,
+        GroceryItemEntity::class,
+        DailyLogEntity::class
+    ],
+    version = 2,
     exportSchema = true
 )
 abstract class CalorieStreakDatabase : RoomDatabase() {
     abstract fun ingredientDao(): IngredientDao
+    abstract fun appDao(): AppDao
 }
