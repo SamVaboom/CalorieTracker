dc1f5d2e6645d5f0735c4ff3b1cb27aa148f9b26
SamVaboom <samuel.vabulari@ggs.ch>
2026-07-13 08:09:18 +0200

    Centralize streak and freeze thresholds

---
+++ app/src/main/java/com/sam/caloriestreak/domain/calculation/StreakRules.kt
@@ -0,0 +1,8 @@
+package com.sam.caloriestreak.domain.calculation
+
+object StreakRules {
+    const val STREAK_SCORE_THRESHOLD = 80.0
+    const val FREEZE_QUALIFYING_SCORE = 85.0
+    const val FREEZE_REQUIRED_DAYS = 7
+    const val MAX_STORED_FREEZES = 3
+}
