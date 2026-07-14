403aa4b66b7b14f3c5f0c5c47866d68f513b80d7
SamVaboom <samuel.vabulari@ggs.ch>
2026-07-14 18:53:37 +0200

    Register weight and achievement storage

--- app/src/main/java/com/sam/caloriestreak/data/local/database/CalorieStreakDatabase.kt
+++ app/src/main/java/com/sam/caloriestreak/data/local/database/CalorieStreakDatabase.kt
@@ -3,13 +3,16 @@ package com.sam.caloriestreak.data.local.database
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.sam.caloriestreak.data.local.dao.AppDao
+import com.sam.caloriestreak.data.local.dao.FeatureDao
 import com.sam.caloriestreak.data.local.dao.IngredientDao
 import com.sam.caloriestreak.data.local.entity.DailyLogEntity
+import com.sam.caloriestreak.data.local.entity.EarnedAchievementEntity
 import com.sam.caloriestreak.data.local.entity.GroceryItemEntity
 import com.sam.caloriestreak.data.local.entity.IngredientEntity
 import com.sam.caloriestreak.data.local.entity.MealLogEntity
 import com.sam.caloriestreak.data.local.entity.RecipeEntity
 import com.sam.caloriestreak.data.local.entity.RecipeItemEntity
+import com.sam.caloriestreak.data.local.entity.WeightEntryEntity
 
 @Database(
     entities = [
@@ -18,12 +21,15 @@ import com.sam.caloriestreak.data.local.entity.RecipeItemEntity
         RecipeItemEntity::class,
         MealLogEntity::class,
         GroceryItemEntity::class,
-        DailyLogEntity::class
+        DailyLogEntity::class,
+        WeightEntryEntity::class,
+        EarnedAchievementEntity::class
     ],
-    version = 2,
+    version = 3,
     exportSchema = true
 )
 abstract class CalorieStreakDatabase : RoomDatabase() {
     abstract fun ingredientDao(): IngredientDao
     abstract fun appDao(): AppDao
+    abstract fun featureDao(): FeatureDao
 }
