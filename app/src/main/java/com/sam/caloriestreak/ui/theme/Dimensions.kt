2b3735674e97c717e279a0552599977aa4e658cc
SamVaboom <samuel.vabulari@ggs.ch>
2026-07-15 16:12:22 +0200

    Add centralized spacing tokens

---
+++ app/src/main/java/com/sam/caloriestreak/ui/theme/Dimensions.kt
@@ -0,0 +1,18 @@
+package com.sam.caloriestreak.ui.theme
+
+import androidx.compose.ui.unit.dp
+
+object AppDimensions {
+    val Space4 = 4.dp
+    val Space8 = 8.dp
+    val Space12 = 12.dp
+    val Space16 = 16.dp
+    val Space20 = 20.dp
+    val Space24 = 24.dp
+    val Space32 = 32.dp
+
+    val ScreenPadding = 16.dp
+    val CardPadding = 16.dp
+    val TouchTarget = 48.dp
+    val ContentMaxWidth = 520.dp
+}
