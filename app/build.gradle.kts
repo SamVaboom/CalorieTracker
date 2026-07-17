e27839de2fcff309a49d5a4fa2a67c0da419b28d
SamVaboom <samuel.vabulari@ggs.ch>
2026-07-17 17:56:14 +0200

    Wire committed schemas into Room migration tests

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,6 +27,8 @@ android {
     }
 
     kotlinOptions { jvmTarget = "17" }
+
+    sourceSets["androidTest"].assets.srcDir("$projectDir/schemas")
 }
 
 room {
@@ -52,6 +54,7 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation(platform("androidx.compose:compose-bom:2024.12.01"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
+    androidTestImplementation("androidx.room:room-testing:2.6.1")
     androidTestImplementation("androidx.test:core-ktx:1.6.1")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
